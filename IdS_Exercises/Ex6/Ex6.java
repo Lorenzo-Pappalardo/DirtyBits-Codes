69812ce87f660fb3dc84ed9c390421d268631a37
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-05-16 09:56:05 +0200

    Tried a new way of printing files

---
+++ IdS_Exercises/Ex6/Ex6.java
@@ -0,0 +1,23 @@
+/*
+    6) Isolare l'esplorazione del file system in una classe apposita FileExplorer,
+    avente come attributo d'istanza proprio il percorso della directory da esplorare.
+    Delegare a questo oggetto tutto ciò che adesso è eseguito dal main tramite metodi statici
+    (caricamento file, esplorazione cartelle, stampa del contenuto ecc...).
+    Fare in modo che l'unica responsabilità del main sia di creare un FileExplorer e di farne uso.
+*/
+
+public class Ex6 {
+    public static void main(String args[]) {
+        if (args.length == 0) {
+            System.err.println("You have to pass a path");
+            System.exit(1);
+        }
+
+        if (args.length > 1) {
+            System.err.println("Passed too many argumets, only one path can be processed");
+            System.exit(2);
+        }
+
+        new FileExplorer(args[0]);
+    }
+}
---
+++ IdS_Exercises/Ex6/FileExplorer.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.util.List;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+
+public class FileExplorer {
+    public FileExplorer(String path) {
+        printPathRecursively(new File(path), 0);
+    }
+
+    private void indent(int indent_depth) {
+        for (int i = 0; i < 4 * indent_depth; i++) {
+            System.out.print(' ');
+        }
+    }
+
+    public void printPathRecursively(File f, int indent_depth) {
+        if (f.isDirectory()) {
+            indent(indent_depth);
+
+            System.out.println("--> " + f.getName() + " is a directory, printing contents below...");
+
+            for (File tmp : f.listFiles()) {
+                printPathRecursively(tmp, indent_depth + 1);
+            }
+        } else
+            printRegularFile(f, indent_depth);
+    }
+
+    private void printRegularFile(File f, int indent_depth) {
+        indent(indent_depth);
+
+        System.out.println("--> " + f.getName() + " is a regular file, printing content below...");
+
+        try {
+            List<String> lines = Files.readAllLines(FileSystems.getDefault().getPath(f.getPath()));
+
+            for (String tmp : lines) {
+                indent(indent_depth + 1);
+                System.out.println(tmp);
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+            System.exit(3);
+        }
+    }
+}
