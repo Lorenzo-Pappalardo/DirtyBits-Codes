2240a2b0f8e4863127da56148e5dbe9f70f5b8c8
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-06-15 12:08:14 +0200

    First exercise on functional programming

---
+++ SE_Exercises/Functional_Programming_Exercises/Ex1.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Insert words, write stop to stop inserting words xD");
+        while (true) {
+            String word = in.nextLine();
+            if (word.equalsIgnoreCase("stop"))
+                break;
+            list.add(word);
+        }
+
+        System.out.println("Insert the beginning substring");
+        String substringToLookFor = in.nextLine();
+        in.close();
+
+        System.out.println(contains(list, substringToLookFor, substringToLookFor.length()));
+    }
+
+    public static List<String> contains(List<String> original, String substring, int dim) {
+        return original.stream().filter(w -> w.substring(0, dim).equals(substring)).collect(Collectors.toList());
+    }
+}
