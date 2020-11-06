31ad320dd973fa97c4470bd8c0c9a681cd6f3457
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-11-06 21:16:39 +0100

    Added E5.java

---
+++ TDSW_Exercises/E5.java
@@ -0,0 +1,67 @@
+/*
+  Scrivere in C o Java un programma che:
+  - Apra una connessione verso l'IP 90.147.166.230 con porta 80
+  - Invii la stringa "GET /pappalardo/prova/05.aux\n"
+  - Dallo stream di byte ricevuti individui i primi (e unici) compresi tra i caratteri [ e ]
+  - Memorizzi questi byte in una variabile stringa denominata "command2"
+  - Invii, su una nuova connessione verso lo stesso server, la stringa command2, terminata da "\r\n"
+  - Scriva i byte ricevuti in risposta sullo standard output
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class E5 {
+  public static void main(String[] args) {
+    final String address = "127.0.0.1";
+    final String command1 = "GET ./e5.txt\r\n";
+    String command2 = "";
+
+    try (Socket socket = new Socket(InetAddress.getByName(address), 80);
+        BufferedReader socketInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter socketOutput = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);) {
+      socketOutput.println(command1);
+
+      String res;
+      Boolean save = false;
+      while ((res = socketInput.readLine()) != null) {
+        int i = 0;
+        while (i < res.length()) {
+          if (res.charAt(i) == ']') {
+            save = !save;
+          }
+          if (save) {
+            command2 += res.charAt(i);
+          }
+          if (res.charAt(i) == '[') {
+            save = !save;
+          }
+          i++;
+        }
+      }
+    } catch (Exception e) {
+      System.err.println(e);
+      e.printStackTrace();
+    }
+
+    command2 += "\r\n";
+
+    try (Socket socket = new Socket(InetAddress.getByName(address), 80);
+        BufferedReader socketInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter socketOutput = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);) {
+      socketOutput.println(command2);
+
+      String res;
+      while ((res = socketInput.readLine()) != null) {
+        System.out.println(res);
+      }
+    } catch (Exception e) {
+      System.err.println(e);
+      e.printStackTrace();
+    }
+  }
+}
