53413441b9cab588576cd1081423e03be822da4e
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-11-08 12:22:53 +0100

    Now this script can even run code in packages

--- TDSW_Exercises/start.ps1
+++ TDSW_Exercises/start.ps1
@@ -2,12 +2,30 @@ if ($args.length -lt 1) {
   throw "You must pass the file's name that you want to run";
 }
 
-$filename = $args[0]
+$path = $args[0]
 
-Remove-Item .\build\$filename.class
+$folder = "";
+$basename = $args[0];
+if ($path[0] -eq '.') {
+  $tmp = $path.ToString().Split("{\}")
+  $i = $tmp.Length
+  $folder = $tmp[$i-2]
+  $basename = $tmp[$i-1].Split("{.}")[0]
+}
 
-javac "$filename.java" -d .\build
+Remove-Item .\build\$basename.class
+
+if ($folder -eq "" -or $folder -eq ".") {
+  javac "$basename.java" -d .\build
+} else {
+  javac $path -d .\build
+}
 
 Set-Location .\build
-java $filename
+if ($folder -eq "" -or $folder -eq ".") {
+  java $basename
+}
+else {
+  java "$folder.$basename"
+}
 Set-Location ..
