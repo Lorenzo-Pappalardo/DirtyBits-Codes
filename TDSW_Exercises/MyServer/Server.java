2c28f0fa1b76db8de8a756356d7bb4542d90f71e
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-11-12 10:43:53 +0100

    Added inspirational quotes

--- TDSW_Exercises/MyServer/Server.java
+++ TDSW_Exercises/MyServer/Server.java
@@ -1,15 +1,25 @@
 package MyServer;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
+import java.util.Random;
 
-public class Main {
-  private static boolean quit = false;
+public class Server {
+  private static boolean quit;
+  private static int visitors;
+
+  Server() {
+    quit = false;
+    visitors = 0;
+  }
 
   public static void main(String[] args) {
     System.out.println("Server started\n");
@@ -23,7 +33,7 @@ public class Main {
 
         String input;
         while ((input = receiveBuffer.readLine()) != null) {
-          handleRequest(sendBuffer, input);
+          handleRequest(clientSocket, sendBuffer, input);
           if (quit)
             break;
         }
@@ -31,23 +41,38 @@ public class Main {
         e.printStackTrace();
       }
       System.out.println("Previous connection closed\n");
+      visitors++;
     }
   }
 
   private static void welcome(PrintWriter sendBuffer) {
     sendBuffer.println("Hi, how can I help you?");
-    sendBuffer.println("[d] Show current server's date\t[q] Quit");
+    sendBuffer.println("[d] Show current server's date\t[i] Inspirational quote\t[q] Quit");
   }
 
-  private static void handleRequest(PrintWriter sendBuffer, String input) {
+  private static void handleRequest(Socket clientSocket, PrintWriter sendBuffer, String input) {
     if (input.length() > 1)
       sendBuffer.println("Unknown command! Please try again.");
-    if (input.equalsIgnoreCase("d")) {
+    else if (input.equalsIgnoreCase("d")) {
       Date date = new Date(System.currentTimeMillis());
       sendBuffer.println("Server's date: " + date);
     } else if (input.equalsIgnoreCase("q")) {
       sendBuffer.println("Have a great day!");
       quit = true;
+    } else if (input.equalsIgnoreCase("e")) {
+      sendBuffer.println("You've found an Easter Egg!");
+      sendBuffer.println("\t\tCongratulations user " + clientSocket.getInetAddress());
+      sendBuffer.println("\t\tYou are the " + visitors + " visitor, today is your lucky day!");
+    } else if (input.equalsIgnoreCase("i")) {
+      try (BufferedReader reader = new BufferedReader(new FileReader(new File("../MyServer/quotes.txt")));) {
+        Random random = new Random();
+        int i = random.nextInt(102);
+        for (int row = 0; row < i; row++)
+          reader.readLine();
+        sendBuffer.println(reader.readLine());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     } else
       sendBuffer.println("Unknown command! Please try again.");
   }
