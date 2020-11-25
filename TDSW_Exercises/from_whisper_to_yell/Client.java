5dd7d5ee490e74bf89a780f1e364d893bba3de27
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-11-25 22:28:53 +0100

    It works but only for the first word sent

--- TDSW_Exercises/from_whisper_to_yell/Client.java
+++ TDSW_Exercises/from_whisper_to_yell/Client.java
@@ -1,6 +1,8 @@
 package from_whisper_to_yell;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -8,36 +10,48 @@ import java.net.InetAddress;
 import java.util.Scanner;
 
 public class Client {
+  private static final int BUFFERSIZE = 1000;
+
   public static void main(String[] args) {
     if (args.length < 2) {
       System.err.println("You must pass server's ip address and port");
       System.exit(1);
     }
 
-    System.out.println("Enter words with lowercase characters");
-
     Scanner scanner = new Scanner(System.in);
 
     while (true) {
-      String word = scanner.nextLine();
-      if (word.equalsIgnoreCase("quit")) {
-        System.out.println("Quitting...");
-        break;
-      }
+      System.out.println("Enter words with lowercase characters");
+      String line = scanner.nextLine();
 
       try (DatagramSocket socket = new DatagramSocket();
-        ByteArrayOutputStream bytesBuffer = new ByteArrayOutputStream();
+          ByteArrayOutputStream bytesBuffer = new ByteArrayOutputStream();
           DataOutputStream stringBuffer = new DataOutputStream(bytesBuffer);) {
-        stringBuffer.writeUTF(scanner.nextLine());
+        stringBuffer.writeUTF(line);
         byte[] packetData = bytesBuffer.toByteArray();
 
         DatagramPacket packet = new DatagramPacket(packetData, packetData.length, InetAddress.getByName(args[0]),
             Integer.parseInt(args[1]));
-            
-            socket.send(packet);
+
+        socket.send(packet);
+
+        if (line.equalsIgnoreCase("quit")) {
+          System.out.println("Quitting...");
+          break;
+        }
+
+        byte[] receiveBuffer = new byte[BUFFERSIZE];
+        DatagramPacket receivedPacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+
+        socket.receive(receivedPacket);
+
+        String receivedData = new DataInputStream(new ByteArrayInputStream(receivedPacket.getData())).readUTF();
+
+        System.out.println("Server: " + receivedData);
       } catch (Exception e) {
         e.printStackTrace();
       }
     }
+    scanner.close();
   }
 }
--- TDSW_Exercises/from_whisper_to_yell/Server.java
+++ TDSW_Exercises/from_whisper_to_yell/Server.java
@@ -1,5 +1,9 @@
 package from_whisper_to_yell;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
@@ -8,19 +12,37 @@ public class Server {
   private static final int BUFFERSIZE = 1000;
 
   public static void main(String[] args) {
-    try (DatagramSocket socket = new DatagramSocket(PORT);) {
-      System.out.println("Socket created" + socket);
+    try (DatagramSocket socket = new DatagramSocket(PORT);
+        ByteArrayOutputStream bytesBuffer = new ByteArrayOutputStream();
+        DataOutputStream stringBuffer = new DataOutputStream(bytesBuffer);) {
+      System.out.println("Socket created");
 
       byte[] buffer = new byte[BUFFERSIZE];
 
       while (true) {
         DatagramPacket receivedPacket = new DatagramPacket(buffer, buffer.length);
 
+        System.out.println("Waiting for packets...");
         socket.receive(receivedPacket);
 
-        System.out.println("Received packet" + receivedPacket);
+        System.out.println("Received packet");
+        System.out.println("from:" + receivedPacket.getAddress() + "\tport : " + receivedPacket.getPort());
+        System.out.println();
 
-        System.out.println(receivedPacket.getData());
+        String packetData = new DataInputStream(new ByteArrayInputStream(receivedPacket.getData())).readUTF();
+
+        if (packetData.equals("quit"))
+          break;
+
+        System.out.println("Received data: " + packetData);
+
+        stringBuffer.writeUTF(packetData.toUpperCase() + '!');
+        byte[] dataToSend = bytesBuffer.toByteArray();
+
+        DatagramPacket packetToSend = new DatagramPacket(dataToSend, dataToSend.length, receivedPacket.getAddress(),
+            receivedPacket.getPort());
+
+        socket.send(packetToSend);
       }
     } catch (Exception e) {
       e.printStackTrace();
