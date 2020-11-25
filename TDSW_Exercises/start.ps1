bfc4a336dbfe0bee42f4ab4ec6a17edef19aed2a
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-11-25 10:46:43 +0100

    Completely refactored script to make use of Split-Path system call, which is super super cool. Now it's compatible with both Windows Powershell (5.1) and Powershell 7.1

--- TDSW_Exercises/start.ps1
+++ TDSW_Exercises/start.ps1
@@ -1,37 +1,37 @@
 if ($args.length -lt 1) {
-  throw "You must pass the file's name that you want to run";
+  throw "You must pass the file's name that you want to run"
 }
 
-$path = $args[0]
-
-$folder = "";
-$basename = $args[0];
-if ($path[0] -eq '.') {
-  $tmp = $path.ToString().Split("{\}")
-  $i = $tmp.Length
-  $folder = $tmp[$i-2]
-  $basename = $tmp[$i-1].Split("{.}")[0]
+$arguments = ""
+for ($i = 1; $i -lt $args.Count; $i++) {
+  $arguments += $args[$i]
+  if ($i -ne $args.Count - 1) {
+    $arguments += " "
+  }
 }
+
+Remove-Item -Recurse .\build
+
+$folder = Split-Path -Path $args[0] -Parent
+$filename = Split-Path -Path $args[0] -Leaf
+$filename = $filename.Split('.')[0]
 
 if ($folder -eq "" -or $folder -eq ".") {
-  javac "$basename.java" -d .\build
-} else {
-  javac $path -d .\build
+  javac "$filename.java" -d .\build
 }
-
-$arguments  = "";
-for($i=1; $i -lt $args.Count; $i++) {
-    $arguments += $args[$i]
-    if ($i -ne $args.Count-1) {
-      $arguments += " "
-    }
+else {
+  javac "$folder\$filename.java" -d .\build
 }
 
 Set-Location .\build
 if ($folder -eq "" -or $folder -eq ".") {
-  java $basename $arguments
+  java $filename $arguments
 }
 else {
-  java "$folder.$basename" $arguments
+  $folder = $folder.Replace('\', '.')
+  if ($folder.Substring(0, 2) -eq "..") {
+    $folder = $folder.Substring(2)
+  }
+  java "$folder.$filename" $arguments
 }
 Set-Location ..
