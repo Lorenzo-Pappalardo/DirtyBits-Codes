5f2e14ae317f0abef79536ffb022bf929bc1a385
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-11-26 10:05:57 +0100

    It works now

--- TDSW_Exercises/from_whisper_to_yell/Server.java
+++ TDSW_Exercises/from_whisper_to_yell/Server.java
@@ -11,15 +11,14 @@ public class Server {
   public static final int PORT = 8080;
   private static final int BUFFERSIZE = 1000;
 
+  String packetData;
+
   public static void main(String[] args) {
-    try (DatagramSocket socket = new DatagramSocket(PORT);
-        ByteArrayOutputStream bytesBuffer = new ByteArrayOutputStream();
-        DataOutputStream stringBuffer = new DataOutputStream(bytesBuffer);) {
+    try (DatagramSocket socket = new DatagramSocket(PORT);) {
       System.out.println("Socket created");
 
-      byte[] buffer = new byte[BUFFERSIZE];
-
       while (true) {
+        byte[] buffer = new byte[BUFFERSIZE];
         DatagramPacket receivedPacket = new DatagramPacket(buffer, buffer.length);
 
         System.out.println("Waiting for packets...");
@@ -36,13 +35,16 @@ public class Server {
 
         System.out.println("Received data: " + packetData);
 
-        stringBuffer.writeUTF(packetData.toUpperCase() + '!');
-        byte[] dataToSend = bytesBuffer.toByteArray();
+        try (ByteArrayOutputStream bytesBuffer = new ByteArrayOutputStream();
+            DataOutputStream stringBuffer = new DataOutputStream(bytesBuffer);) {
+          stringBuffer.writeUTF(packetData.toUpperCase() + '!');
+          byte[] dataToSend = bytesBuffer.toByteArray();
 
-        DatagramPacket packetToSend = new DatagramPacket(dataToSend, dataToSend.length, receivedPacket.getAddress(),
-            receivedPacket.getPort());
+          DatagramPacket packetToSend = new DatagramPacket(dataToSend, dataToSend.length, receivedPacket.getAddress(),
+              receivedPacket.getPort());
 
-        socket.send(packetToSend);
+          socket.send(packetToSend);
+        }
       }
     } catch (Exception e) {
       e.printStackTrace();
