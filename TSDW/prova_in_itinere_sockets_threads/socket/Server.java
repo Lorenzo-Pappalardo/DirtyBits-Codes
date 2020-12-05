bbac29e1e64ae85eb0e3959ae3eed7abb3870fdd
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-12-05 17:51:09 +0100

    Sockets

---
+++ TSDW/prova_in_itinere_sockets_threads/socket/Server.java
@@ -0,0 +1,43 @@
+package prova_in_itinere_sockets_threads.socket;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+  private static final int PORT = 8080;
+
+  private static int maxLength = 0;
+
+  public static void main(String[] args) {
+    System.out.println("Server started");
+
+    try (ServerSocket socket = new ServerSocket(PORT);
+        Socket clientSocket = socket.accept();
+        BufferedReader receiveBuffer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter sendBuffer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true);) {
+      System.out.println("Connection from " + clientSocket + " accepted");
+
+      // sendBuffer.println("Insert word: "); // This is needed to use telnet
+
+      while (true) {
+        String request = receiveBuffer.readLine();
+        if (request.equals("exit"))
+          break;
+
+        if (request.length() > maxLength) {
+          maxLength = request.length();
+        }
+      }
+
+      sendBuffer.println("Max Length: " + maxLength);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    System.out.println("Server stopped");
+  }
+}
