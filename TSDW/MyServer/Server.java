4a0ab463aa9b0180ac6c9bf0c0fb083b1cc81239
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-12-06 14:16:57 +0100

    Removed sample client, telnet is just better. Implemented chat functionality.

--- TSDW/MyServer/Server.java
+++ TSDW/MyServer/Server.java
@@ -11,13 +11,12 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Server {
-  private static boolean quit;
   private static int visitors;
 
   Server() {
-    quit = false;
     visitors = 0;
   }
 
@@ -29,51 +28,83 @@ public class Server {
           BufferedReader receiveBuffer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
           PrintWriter sendBuffer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true)) {
         System.out.println("Connection accepted: " + clientSocket);
-        welcome(sendBuffer);
+        visitors++;
 
-        String input;
-        while ((input = receiveBuffer.readLine()) != null) {
-          handleRequest(clientSocket, sendBuffer, input);
-          if (quit)
-            break;
-        }
+        sendBuffer.println("Hi " + clientSocket.getInetAddress() + " xD\t\t\t You're visitor n." + visitors);
+        sendCommands(sendBuffer);
+
+        handleRequest(clientSocket, receiveBuffer, sendBuffer);
       } catch (Exception e) {
         e.printStackTrace();
       }
       System.out.println("Previous connection closed\n");
-      visitors++;
     }
   }
 
-  private static void welcome(PrintWriter sendBuffer) {
-    sendBuffer.println("Hi, how can I help you?");
-    sendBuffer.println("[d] Show current server's date\t[i] Inspirational quote\t[q] Quit");
+  private static void sendCommands(PrintWriter sendBuffer) {
+
+    sendBuffer.println("\nHow can I help you?");
+    sendBuffer.println("[d] Show current server's date   [i] Inspirational quote   [c] Chat   [q] Quit");
   }
 
-  private static void handleRequest(Socket clientSocket, PrintWriter sendBuffer, String input) {
-    if (input.length() > 1)
-      sendBuffer.println("Unknown command! Please try again.");
-    else if (input.equalsIgnoreCase("d")) {
-      Date date = new Date(System.currentTimeMillis());
-      sendBuffer.println("Server's date: " + date);
-    } else if (input.equalsIgnoreCase("q")) {
-      sendBuffer.println("Have a great day!");
-      quit = true;
-    } else if (input.equalsIgnoreCase("e")) {
-      sendBuffer.println("You've found an Easter Egg!");
-      sendBuffer.println("\t\tCongratulations user " + clientSocket.getInetAddress());
-      sendBuffer.println("\t\tYou are the " + visitors + " visitor, today is your lucky day!");
-    } else if (input.equalsIgnoreCase("i")) {
-      try (BufferedReader reader = new BufferedReader(new FileReader(new File("../MyServer/quotes.txt")));) {
-        Random random = new Random();
-        int i = random.nextInt(102);
-        for (int row = 0; row < i; row++)
-          reader.readLine();
-        sendBuffer.println(reader.readLine());
-      } catch (IOException e) {
-        e.printStackTrace();
+  private static void handleRequest(Socket clientSocket, BufferedReader receiveBuffer, PrintWriter sendBuffer)
+      throws IOException {
+    String input;
+    while ((input = receiveBuffer.readLine()) != null) {
+      sendBuffer.println();
+
+      if (input.length() > 1)
+        sendBuffer.println("Unknown command! Please try again.");
+
+      else if (input.equalsIgnoreCase("d")) {
+        Date date = new Date(System.currentTimeMillis());
+        sendBuffer.println("Server's date: " + date);
       }
-    } else
-      sendBuffer.println("Unknown command! Please try again.");
+
+      else if (input.equalsIgnoreCase("q")) {
+        sendBuffer.println("Have a great day!");
+        break;
+      }
+
+      else if (input.equalsIgnoreCase("e")) {
+        sendBuffer.println("You've found an Easter Egg!");
+        sendBuffer.println("\t\tCongratulations user " + clientSocket.getInetAddress());
+        sendBuffer.println("\t\tYou are the " + visitors + " visitor, today is your lucky day!");
+      }
+
+      else if (input.equalsIgnoreCase("i")) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("../MyServer/quotes.txt")));) {
+          int i = new Random().nextInt(102);
+          for (int row = 0; row < i; row++)
+            reader.readLine();
+          sendBuffer.println(reader.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+
+      else if (input.equalsIgnoreCase("c")) {
+        Scanner keyboardInput = new Scanner(System.in);
+        System.out.println("\n\n----------Chat started----------\n");
+        sendBuffer.println("\n\n----------Chat started----------\n");
+        while (true) {
+          String line = receiveBuffer.readLine();
+          if (line.equalsIgnoreCase("exit")) {
+            System.out.println("\n\n----------Chat stopped----------\n");
+            sendBuffer.println("\n\n----------Chat stopped----------\n");
+            break;
+          }
+          System.out.println("Received: " + line);
+          System.out.print("Enter reply: ");
+          sendBuffer.println("Server's reply: " + keyboardInput.nextLine());
+        }
+        keyboardInput.close();
+      }
+
+      else
+        sendBuffer.println("Unknown command! Please try again.");
+
+      sendCommands(sendBuffer);
+    }
   }
 }
