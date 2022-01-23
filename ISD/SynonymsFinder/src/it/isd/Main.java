b0719f48aab560ba81b373e9d54c00db1bb2cbea
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2022-01-23 16:56:59 +0100

    Added project for Ingegneria dei Sistemi Distribuiti

---
+++ ISD/SynonymsFinder/src/it/isd/Main.java
@@ -0,0 +1,14 @@
+package it.isd;
+
+import it.isd.proxy.KeyValuePairsFile;
+import it.isd.proxy.KeyValuePairsFileProxy;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+  public static void main(String[] args) {
+  }
+}
